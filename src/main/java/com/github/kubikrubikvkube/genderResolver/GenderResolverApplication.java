89946fd34122cae4904297f5f761ca99a8a5e119
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-08-22 15:25:45 +0300

    Started

---
+++ src/main/java/com/github/kubikrubikvkube/genderResolver/GenderResolverApplication.java
@@ -0,0 +1,13 @@
+package com.github.kubikrubikvkube.genderResolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenderResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kubikrubikvkube/genderResolver/GenderResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kubikrubikvkube.genderResolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bc8930cad74c5e56d345b9adca90d3e4ea6c8af2
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-08-23 18:55:01 +0300

    WORKS

--- src/main/java/com/github/kubikrubikvkube/genderResolver/name/NameRepository.java
+++ src/main/java/com/github/kubikrubikvkube/genderResolver/name/NameRepository.java
@@ -1,13 +1,15 @@
 package com.github.kubikrubikvkube.genderResolver.name;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.Repository;
 
+import javax.persistence.Table;
 import java.util.Collection;
 
-@Repository
-public interface NameRepository extends CrudRepository<NameEntity, Long> {
+@org.springframework.stereotype.Repository
+@Table(name = "russian_names", schema = "public")
+public interface NameRepository extends Repository<NameEntity, Long> {
+
     @Query(value = "SELECT * FROM public.russian_names", nativeQuery = true)
     Collection<NameEntity> foo();
 }

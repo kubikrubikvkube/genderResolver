d1e37782209962b12cf6bef7d9e6f0f5c81dff38
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-08-27 16:42:12 +0300

    Encoding problems solved. Now this component can be launched as JAR file

--- src/main/java/com/github/kubikrubikvkube/genderResolver/GenderResolverApplication.java
+++ src/main/java/com/github/kubikrubikvkube/genderResolver/GenderResolverApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class GenderResolverApplication {
+public class GenderResolverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GenderResolverApplication.class, args);
